ff93fa84386c07210e0280b58b63f0c118d1f467
Cristobal Sfeir <cnsfeir@uc.cl>
2023-04-15 22:00:15 -0400

    ⚗️ Try new deploy method

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import { Request, Response, http } from '@google-cloud/functions-framework'
+import { HttpFunction, Request, Response } from '@google-cloud/functions-framework'
 import getAuthenticationCookie from './integrations/cumplo'
 
-http('TypescriptFunction', (request: Request, response: Response) => {
+export const getAuthorizationCookie: HttpFunction = (request: Request, response: Response) => {
   const cookie = getAuthenticationCookie()
   if (!cookie) {
     response.status(500).send('Could not get cookie')
     return
   }
   response.status(200).json({ cookie })
-})
+}
